c48811d6afd03965fc30723753de0d5424a324d1
Crystal Xu <u6191391@anu.edu.au>
2017-10-13 21:56:07 +1100

    g

--- assigment.py
+++ assigment.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 import matplotlib.pyplot as mpl
 #import csv
 #with open('Rainfall_Canberra_070247.csv') as csvfile_Canberra:
@@ -8,7 +9,7 @@ import matplotlib.pyplot as mpl
 #        print(row)
         
 
-#select the path to the file to be read
+# select the path to the file to be read
 file_index = input('select weather observation station: \n1. Sydney \n2. Canberra \n3. Queanbeyan \n')
 if file_index == '1':
     file = 'Rainfall_Sydney_066062.csv'
@@ -18,31 +19,48 @@ if file_index == '3':
     file = 'Rainfall_Queanbeyan_070072.csv'
 data = pd.read_csv(file)
 
-#select month
+# select month
 month = int(input('Select the month: \n1. January \n2. February \n3. March \n4. April \n5. May \n6. June \n7. July \n8. August \n9. September \n10. October \n11. November \n12. December \n'))
 
-#select the type of time series aggregation
-#time = int(input('Select time series aggregation (it should be an integer): '))
+# select the type of time series aggregation
+time = int(input('Select time series aggregation (it should be an integer): '))
+
+
+
 
 # draw table of total rainfall amount every month in every year
 table = pd.pivot_table(data, values='Rainfall amount (millimetres)', index=['Month'], columns=['Year'], aggfunc=np.sum)
 
-# corresponding list of total rainfall values
-y = list(table.loc[(month)].values) 
+# clist of total rainfall values
+rainfall_month = list(table.loc[(month)].values)
 
 # list of years in the table above
-years = list(table.columns) 
+years = list(table.columns)
+
+
+# find threshold line
+entries = len(rainfall_month)
+
+once_threshold = math.floor(entries * (1/time))
+
+rainfall_month_sort = rainfall_month[:]
+
+rainfall_month_sort.sort()
+
+threshold_line = rainfall_month_sort[- once_threshold]
+    
 
 # bar chart
+y = rainfall_month
 x = np.arange(0, len(y))
 mpl.bar(x + 0.25, y, 0.5, color='blue')
-mpl.plot([0, len(y) + 1], [143.8, 143.8], '--k')  # the threshold line
+mpl.plot([0, len(y) + 1], [threshold_line, threshold_line], '--k')  # the threshold line
 mpl.xticks(x + 0.5, years, rotation=90)
 mpl.show()
 
 # scatter plot
 mpl.plot(x, y, '.b', markersize=5)
-mpl.plot([0, len(y) + 1], [143.8, 143.8], '--k')
+mpl.plot([0, len(y) + 1], [threshold_line, threshold_line], '--k')
 mpl.xticks(x, years, rotation=90)
 mpl.show()
 
