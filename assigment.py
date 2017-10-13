e0806295a275d20a3e0058b68cae12dec3a03453
Crystal Xu <u6191391@anu.edu.au>
2017-10-13 19:29:51 +1100

    tt
    t

--- assigment.py
+++ assigment.py
@@ -1,5 +1,5 @@
-import csv
-#import numpy as np
+#import csv
+import numpy as np
 #with open('Rainfall_Canberra_070247.csv') as csvfile_Canberra:
 #    reader = csv.reader(csvfile_Canberra, delimiter=',', quoting=csv.QUOTE_NONE)
 #    for row in reader:
@@ -9,19 +9,24 @@ import pandas as pd
 
 data = pd.read_csv('Rainfall_Canberra_070247.csv')
 
-june = data[data['Month']==6]
+#june = data[data['Month']==6]
 
-sum_june = june['Rainfall amount (millimetres)'].sum()
+#sum_june = june['Rainfall amount (millimetres)'].sum()
 
-number_of_years = pd.value_counts(data['Year']).index.tolist()
-number_of_years.sort()
+#number_of_years = pd.value_counts(data['Year']).index.tolist()
+#number_of_years.sort()
 
-canberra_june_annual = []
+#canberra_june_annual = []
 
-for year in number_of_years:
-    canberra_june_annual.append(data[data['Year']==year][data[data['Year']==year]['Month']==6]['Rainfall amount (millimetres)'].sum())
+#for year in number_of_years:
+    #canberra_june_annual.append(data[data['Year']==year][data[data['Year']==year]['Month']==6]['Rainfall amount (millimetres)'].sum())
 
-table = pd.pivot_table(data, values='Rainfall amount (millimetres)', index=['Year', 'Month'],aggfunc=np.sum)
+def month_total(year, month):
+    table_month = pd.pivot_table(data, values='Rainfall amount (millimetres)', index=['Year', 'Month'],aggfunc=np.sum)
 
-table.loc[(2017, 1)].values[0]
+    return table_month.loc[(year, month)].values[0]
 
+def year_total(year):
+    table_year = pd.pivot_table(data, values='Rainfall amount (millimetres)', index=['Year'],aggfunc=np.sum)
+
+    return table_year.loc[(year)].values[0]
